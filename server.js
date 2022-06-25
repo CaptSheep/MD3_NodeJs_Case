85f7e094f0c0f2905c8973f90dcd73e90dcbeaad
trieu.long <long.trieu.hr@gmail.com>
2022-06-25 22:32:18 +0700

    nodejs

--- server.js
+++ server.js
@@ -1,41 +1,74 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const app = express()
-app.use(express.json());
-const users = [];
-app.use(express.static('public'));
-app.use(express.static('files'))
-app.set('view engine', 'html');
-app.engine('html', require('ejs').renderFile);
-app.get('/', (req, res) => {
-        res.render('index.html')
-    })
-    // app.post('/users', async(req, res) => {
-    //     try {
-    //         const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    //         const user = { name: req.body.name, password: hashedPassword }
-    //         users.push(user)
-    //         res.status(201);
-    //         res.send();
-    //     } catch {
-    //         res.send(500)
-    //         res.send()
-    //     }
-    // })
-    // app.post('/login', async(req, res) => {
-    //     const user = users.find(user => user.name === req.body.name);
-    //     if (user == null) {
+const mysql = require('mysql');
+const http = require('http');
+const qs = require('qs');
+const url = require("url");
+const fs = require('fs');
+const util = require('util');
 
-//         return res.status(400).send('Cannot find User')
-//     }
-//     try {
-//         if (await bcrypt.compare(req.body.password, user.password)) {
-//             res.send('Success')
-//         } else {
-//             res.send('Wrong')
-//         };
-//     } catch {
-//         res.status(500).send()
-//     }
-// })
-app.listen(8000)
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    port: '8889',
+    database: 'shopping_web',
+    charset: 'utf8_general_ci',
+    // socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+});
+const query = util.promisify(connection.query).bind(connection);
+
+connection.connect(function(err) {
+    if (err) {
+        console.log(err);;
+    } else {
+        console.log('DB Connection Success')
+    }
+})
+
+const server = http.createServer(async(res, req) => {
+    require('dotenv').config()
+    let pathUrl = url.parse(req.url).pathname;
+    switch (pathUrl) {
+        case '/':
+            if (req.method === "GET") {
+                await showListPosts(req, res);
+            }
+            break;
+
+    }
+
+})
+async function showListPosts() {
+    let result;
+    let sql = 'SELECT * FROM posts';
+    try {
+        result = await query(sql);
+    } catch (err) {
+        console.log(err);
+    }
+    fs.readFile('./views/index.html', 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            let html = ''
+            result.forEach(items => {
+                html += '<tr>'
+                html += `<td>${items.id}</td>`
+                html += `<td>${items.title}</td>`
+                html += `<td>${items.content}</td>`
+                html += `<td>${items.userId}</td>`
+                html += `<td>${items.image}</td>`
+                html += `<td>${items.statusId}</td>`
+                html += `<td>${items.price}</td>`
+                html += `<td>${items.quantity}</td>`
+                html += `<td><a href="/delete1?id=${items.id}">delete</a></td>`
+                html += `<td><a href="/update?id=${items.id}">update</a></td>`
+                html += '</tr>'
+            })
+        }
+
+
+    })
+}
+server.listen(8000, () => {
+    console.log('Server is running in port 8000')
+})
